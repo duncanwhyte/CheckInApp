1983a0388c51115b19d1a067f3d5669bbc0a76bc
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-11-07 15:29:38 +0000

    feature: Created UserCard component which wil represent users

---
+++ checkinapp/src/app/components/UserCard/index.tsx
@@ -0,0 +1,10 @@
+export default function UserCard({name, jobTitle, present, avatar}) {
+    return (
+        <li className="w-full ">
+            <div className="bg-[#F0F0F0] px-4 py-2">
+
+            </div>
+        </li>
+    )
+    
+}
