3fbb773d7802620083895184fa78e5e2de7a297b
Duncan Whyte <duncan_whyte@hotmail.co.uk>
2024-11-07 15:21:25 +0000

    feat: Created TaxAssistHeader component to include in the layout component.

---
+++ checkinapp/src/app/components/TaxAssistHeader/index.tsx
@@ -0,0 +1,7 @@
+export default function TaxAssistHeader() {
+    return (
+    <header className="bg-[#005CBE] px-6 py-3 rounded-b-lg">
+        <h1 className="text-3xl text-white">Tax <span className="text-[#FFE204]">Assist</span> Attendance</h1>
+    </header>
+    )
+}
--- checkinapp/src/app/layout.tsx
+++ checkinapp/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import TaxAssistHeader from "./components/TaxAssistHeader";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body
         className=""
       >
+        <TaxAssistHeader />
         {children}
       </body>
     </html>
